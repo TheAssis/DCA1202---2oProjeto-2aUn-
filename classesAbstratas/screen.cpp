7b3621f217aa79c459a0b24999a7ce2c41c80d03
Mateus de Assis Silva <33188461+TheAssis@users.noreply.github.com>
2018-10-16 15:04:17 -0300

    definição da matriz responsável pela tela
    Aqui se define a matriz responsável por desenhar a tela.

--- classesAbstratas/screen.cpp
+++ classesAbstratas/screen.cpp
@@ -1,9 +1,10 @@
 //screen.cpp -- Aqui são implementados os métodos da classe Screen
 #include "screen.h"
+#include <vector>       //para poder usar vector
 
-Screen::Screen() {}       //construtor padrão
-
-
+Screen::Screen() {       //construtor padrão
+    mat = vector< vector<char> >(nlin, vector<char>(ncol,brush)); // definindo a matriz tela
+}
 void Screen::setBrush(char _brush) {
     brush = _brush;
 }
