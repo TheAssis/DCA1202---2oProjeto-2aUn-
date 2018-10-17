dbc5ae0ac5930d620beec9af3a861a845bb8bd2e
Mateus de Assis Silva <33188461+TheAssis@users.noreply.github.com>
2018-10-17 17:57:19 -0300

    correção do construtor padrão

--- classesAbstratas/screen.cpp
+++ classesAbstratas/screen.cpp
@@ -2,7 +2,9 @@
 #include "screen.h"
 #include <vector>       //para poder usar vector
 
-Screen::Screen() {       //construtor padrão
+Screen::Screen(int _nlin,int _ncol) {       //construtor padrão
+    nlin = _nlin;
+    ncol = _ncol;
     mat = vector< vector<char> >(nlin, vector<char>(ncol,brush)); // definindo a matriz tela
 }
 void Screen::setBrush(char _brush) {
