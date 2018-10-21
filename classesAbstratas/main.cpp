57ee0173fad032a13516815ffb754759c6d2c07f
Ariel Alsina <ariel.alsina2007@gmail.com>
2018-10-21 01:32:22 -0300

    Alterações secundárias ao projeto (irrelevantes).

--- classesAbstratas/main.cpp
+++ classesAbstratas/main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include "screen.h"
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello World!" << endl;
+int main() {
+    //testando se está printando.
+    Screen tela(20,30);
+
     return 0;
 }
