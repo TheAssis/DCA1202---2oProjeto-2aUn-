72a1895a0aac29ee9c1b8467091d7e52a735d086
Mateus de Assis Silva <33188461+TheAssis@users.noreply.github.com>
2018-10-25 17:50:05 -0300

    Melhorias adicionadas
    Criei novas variáveis de estado: x0, y0, altura, largura, fillmode e brush. Também adicionei um construtor com argumentos, a ser usado pelo Parser.

--- classesAbstratas/retangulo.h
+++ classesAbstratas/retangulo.h
@@ -6,10 +6,12 @@
 
 class Retangulo : public FiguraGeometrica
 {
-
+private:
+    int x0, y0, largura, altura, fillmode;
+    char brush;
 public:
     Retangulo();
-
+    Retangulo(int _x0,int _y0,int _largura,int _altura,int _fillmode,char _brush);
 };
 
 #endif // RETANGULO_H
