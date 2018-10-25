4e51f7065f5523c4139c56bd3c8bdc29b1462b61
Mateus de Assis Silva <33188461+TheAssis@users.noreply.github.com>
2018-10-25 17:58:33 -0300

    Melhorias Adicionadas
    Definição de novo construtor, com argumentos.

--- classesAbstratas/circulo.cpp
+++ classesAbstratas/circulo.cpp
@@ -1,4 +1,13 @@
 //circulo.cpp -- Implementações dos métodos da classe Circulo
 #include "circulo.h"
 
-Circulo::Circulo() {}       //construtor padrão.
+Circulo::Circulo() {}
+
+Circulo::Circulo(int _x0, int _y0, int _raio, int _fillmode, char _brush)
+{
+    x0 = _x0;
+    y0 = _y0;
+    raio = _raio;
+    fillmode = _fillmode;
+    brush = _brush;
+}       //construtor padrão.
