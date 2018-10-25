d6151cd4ebe19a8a71c2c70889af0e7e9f4513cd
Mateus de Assis Silva <33188461+TheAssis@users.noreply.github.com>
2018-10-25 17:48:23 -0300

    Melhorias adicionadas
    Defini um novo construtor (com argumentos, a ser usado pelo parser). Nele defini novas variáveis de estado: x0,y0,altura, largura, fillmode e brush.

--- classesAbstratas/retangulo.cpp
+++ classesAbstratas/retangulo.cpp
@@ -1,4 +1,14 @@
 //retangulo.cpp -- Implementações dos métodos da classe Retangulo
 #include "retangulo.h"
 
-Retangulo::Retangulo() {}       //construtor padrão
+Retangulo::Retangulo() {}
+
+Retangulo::Retangulo(int _x0, int _y0, int _largura, int _altura, int _fillmode, char _brush)
+{
+    x0 = _x0;
+    y0 = _y0;
+    largura = _largura;
+    altura = _altura;
+    fillmode = _fillmode;
+    brush = _brush;
+}       //construtor padrão
