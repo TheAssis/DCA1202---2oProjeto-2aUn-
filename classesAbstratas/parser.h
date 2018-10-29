ad40c7ebf222a275d7e4f6bbb2f12a8d8db14a2d
Mateus de Assis Silva <33188461+TheAssis@users.noreply.github.com>
2018-10-29 17:20:05 -0300

    Parser incompleto
    header do parser.

--- classesAbstratas/parser.h
+++ classesAbstratas/parser.h
@@ -1,18 +1,23 @@
+
 #ifndef PARSER_H
 #define PARSER_H
 
 #include <iostream> //as strings existem no escopo do std
 #include <string> // para string
-
+#include <vector> // vetor de figuras geométricas
+#include "screen.h"
+#include "figurageometrica.h"
 using namespace std;
 
 class Parser{
 private:
     string caminho = string(); //caminho pro arquivo
+    vector < FiguraGeometrica* > figuras ;
 public:
     Parser(); //construtor sem argumentos
     Parser(string _caminho);
-    void ler_arquivo(string caminho);
+    void exec(string _caminho);
+    void gravar();
 };
 
 #endif // PARSER_H
