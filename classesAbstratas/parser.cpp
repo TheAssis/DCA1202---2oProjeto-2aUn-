52ebdcb30c7b1f4fca3f19ba108a90c5feffd485
Mateus de Assis Silva <33188461+TheAssis@users.noreply.github.com>
2018-10-29 17:19:17 -0300

    Leitura de arquivo concluída
    O código aqui disposto é responsável por ler um arquivo, armazenar as instruções de desenho e, em seguida desenhar. Entretanto, o processo de imprimir ainda precisa ser atualizado, de forma a permitir a disposição em tela e a escrita em arquivos .txt .

--- classesAbstratas/parser.cpp
+++ classesAbstratas/parser.cpp
@@ -4,13 +4,15 @@
 #include <string> // para string
 #include "parser.h"
 
-#include "screen.h"
-#include "circulo.h"
-#include "reta.h"
-#include "retangulo.h"
-#include "figurageometrica.h"
+#include "screen.h" //desenha-senuma tela
+#include "circulo.h" // crio objetos circulo
+#include "reta.h" // cria-se retas
+#include "retangulo.h" //cria-se retangulos
+#include "figurageometrica.h" //usa-se um vetor de figuras geometricas
 
 using namespace std; //strings no escopo do std
+
+
 
 Parser::Parser(){
 
@@ -20,8 +22,8 @@ Parser::Parser(string _caminho){
     caminho = _caminho;
 }
 
-void Parser::ler_arquivo(string caminho){
-
+void Parser::exec(string _caminho){
+    caminho = _caminho;
     ifstream fin; //objeto conectado ao fluxo de saída
     string linha; //linha de comando encontrada no arquivo .txt
     string cmd;   //comando encontrado no arquivo .txt
@@ -33,7 +35,6 @@ void Parser::ler_arquivo(string caminho){
         cout << "Falha na abertura do arquivo. Caminho especificado correto? \n";
     }
 
-
     while(fin.good()){
         getline(fin,linha);
         if(!fin.good()){
@@ -42,43 +43,68 @@ void Parser::ler_arquivo(string caminho){
         linha += " ";
         ss.str(linha);//ss = stringstream(linha);
         ss >> cmd;
-        cout << cmd << endl;
+       // cout << cmd << endl; //RETIRAR
 
-        if(cmd.compare("dim") == 0){
+        if(cmd.compare("dim") == 0){ // FALTA TRABALHAR AQUI
             int larg, alt;
             ss >> larg >> alt;
             tela = Screen(larg, alt);
-
         }
         else if(cmd.compare("rectangle") == 0){
             int x0, y0, larg, alt, tipo;
             string brush;
             ss >> x0 >> y0 >> larg >> alt >> tipo;
             ss >> brush;
-            if(ss.good()){
-                cout << "brush = " << brush << endl;
-            }
-            else{
+            if(!ss.good()){
+              //  cout << "brush = " << brush << endl; //RETIRAR
                 brush = " ";
-                cout << "brush = espaco\n";
             }
-            //    armazenamento = new Rectangle(x0, y0, larg, alt, tipo, brush);
+
+            const char* local_brush = brush.c_str(); // conversão necessária: string -> const char* -> const char
+            figuras.push_back(new Retangulo(x0,y0,larg, alt, tipo, *local_brush));
         }
+
         else if(cmd.compare("circle") == 0){
             int x0, y0, raio, tipo;
             string brush;
             ss >> x0 >> y0 >> raio >> tipo;
             ss >> brush;
-            if(ss.good()){
-                cout << "brush = " << brush << endl;
-            }
-            else{
+            if(!ss.good()){
+              //  cout << "brush = " << brush << endl; //RETIRAR
                 brush = " ";
-                cout << "brush = espaco\n";
             }
+
+            const char* local_brush = brush.c_str(); // conversão necessária: string -> const char* -> const char
+            figuras.push_back(new Circulo(x0,y0,raio, tipo, *local_brush));
+        }
+        else if(cmd.compare("line") == 0){
+           int x0, y0, x1, y1;
+           string brush;
+           ss >> x0 >> y0 >> x1 >> y1;
+           ss >> brush;
+           if(!ss.good()){
+             //  cout << "brush = " << brush << endl; //RETIRAR
+               brush = " ";
+           }
+
+           const char* local_brush = brush.c_str(); // conversão necessária: string -> const char* -> const char
+           figuras.push_back(new Reta(x0,y0,x1,y1, *local_brush));
         }
 
     }
 
     fin.close();
+    // fim da leitura, agora irei desenhar
+
+    for(int i = 0; i < figuras.size(); i++){
+    //cout<<"ja cheguei no desenhador";
+        figuras[i]->draw(tela);
+    }
+    cout<<tela; // isso aqui vai pro Parser::gravar
+
+}
+
+void Parser::gravar()
+{
+// código responsável por printar na tela ou gravar em txt. Ou seja, conecta o resultado do desenho a um fluxo de saída
 }
