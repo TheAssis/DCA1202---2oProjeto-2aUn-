bfb39c3a7a0814ec2978db424542d174fa5d958b
Mateus de Assis Silva <33188461+TheAssis@users.noreply.github.com>
2018-10-29 17:23:58 -0300

    Teste do parser adicionado
    Foi adicionado o teste do método parser.exec() .

--- classesAbstratas/main.cpp
+++ classesAbstratas/main.cpp
@@ -9,14 +9,14 @@ using namespace std;
 
 int main() {
     //testando se está printando.
-    Screen tela(20,30);
+    Screen tela(20,20);
 
     tela.setBrush('&');
     tela.setPixel(0,1);
 
     //cout << tela;
 
-    Retangulo *ret = new Retangulo (0,2,15,9,10,'*');
+    Retangulo *ret = new Retangulo (0,2,5,5,10,'*');
     ret->draw(tela);
 
     Reta *reta = new Reta (0, 0, 10, 10, '%');
@@ -30,7 +30,8 @@ int main() {
     delete reta;
     delete ret;
 
-    Parser leitor ("/home");
+    Parser leitor;
+    leitor.exec("C:\\Users\\SALA DE AULA\\Documents\\Assis\\teste.txt");
 
     return 0;
 }
