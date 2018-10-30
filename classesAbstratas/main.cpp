d9d0e2e29beb8768560744b41eeed74d61fbaaca
Mateus de Assis Silva <33188461+TheAssis@users.noreply.github.com>
2018-10-30 19:48:26 -0300

    comentários na função exemplo
    foram adicionados comentários que instruem como usar a função exemplo.

--- classesAbstratas/main.cpp
+++ classesAbstratas/main.cpp
@@ -11,7 +11,7 @@
 using namespace std;
 
 int main() {
-    //testando se está printando.
+    //teste de desenho via função principal (main).
     Screen tela(35,35);
 
     tela.setBrush('&');
@@ -30,9 +30,9 @@ int main() {
 
     cout << tela;
 
-    ofstream fout; //para teste de impressão em txt
-    fout.open("C:\\Users\\SALA DE AULA\\Documents\\Assis\\registro_de_impressao.txt");
-    if(!fout.is_open()){
+    ofstream fout; //para teste de impressão em txt do que foi colocado via função principal
+    fout.open("C:\\Users\\SALA DE AULA\\Documents\\Assis\\registro_de_impressao.txt"); //caminho do arquivo onde vai ser
+    if(!fout.is_open()){                                                               //desenhado
         cout << "arquivo nao foi aberto";
         exit(0);
     }
@@ -43,20 +43,25 @@ int main() {
     delete reta;
     delete ret;
 
+    //fim do teste via função main
+    
+    //início do teste via parser
     Parser leitor;
-    leitor.exec("C:\\Users\\SALA DE AULA\\Documents\\Assis\\teste.txt");
+    leitor.exec("C:\\Users\\SALA DE AULA\\Documents\\Assis\\teste.txt"); //caminho do arquivo onde estarão as instruções de desenho
     cout<<tela;
 
     Screen telaFILE(20,20);
 
     ofstream foutFILE; //para teste de impressão em txt
-    foutFILE.open("C:\\Users\\SALA DE AULA\\Documents\\Assis\\registro_de_impressaoFILE.txt");
-    if(!foutFILE.is_open()){
+    foutFILE.open("C:\\Users\\SALA DE AULA\\Documents\\Assis\\registro_de_impressaoFILE.txt"); //caminho do arquivo onde vai ser
+    if(!foutFILE.is_open()){                                                                   //desenhado.
         cout << "arquivo sdajlfkshfjlkçh nao foi aberto";
         exit(0);
     }
     else{
         foutFILE<<leitor.gravar(telaFILE);
     }
+    
+    //fim do teste via parser
     return 0;
 }
